dad967bbf92ee308ed66a1d0a1d7b51640f6f566
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-08 11:51:25 -0300

    i add bank.entity.ts into bank folder into folder domain to project

---
+++ src/domain/bank/bank.entity.ts
@@ -0,0 +1,9 @@
+export interface BankEntity {
+    ban_uuid: string;
+    ban_name: string;
+    ban_description: string;
+    ban_createdat: Date;
+    ban_updatedat: Date;
+}
+
+export type BankUpdateData = Pick<BankEntity, 'ban_name' | 'ban_description'>;
