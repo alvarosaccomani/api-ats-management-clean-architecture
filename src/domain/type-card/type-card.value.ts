22c99a0d77ea136aee833ec0c0631ea1f8717d64
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-08 11:58:03 -0300

    i add type-card.value.ts into type-card folder into folder domain to project

---
+++ src/domain/type-card/type-card.value.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { TypeCardEntity } from "./type-card.entity";
+
+export class TypeCardValue implements TypeCardEntity {
+    tycrd_uuid: string;
+    tycrd_name: string;
+    tycrd_description: string;
+    tycrd_createdat: Date;
+    tycrd_updatedat: Date;
+    
+    constructor({
+            tycrd_uuid,
+            tycrd_name,
+            tycrd_description,
+            tycrd_createdat,
+            tycrd_updatedat
+        }:{ 
+            tycrd_uuid?: string,
+            tycrd_name: string,
+            tycrd_description?: string,
+            tycrd_createdat?: Date,
+            tycrd_updatedat?: Date
+        }) {
+        this.tycrd_uuid = tycrd_uuid ?? uuid();
+        this.tycrd_name = tycrd_name;
+        this.tycrd_description = tycrd_description ?? '';
+        this.tycrd_createdat = tycrd_createdat ?? moment().toDate();
+        this.tycrd_updatedat = tycrd_updatedat ?? moment().toDate();
+    }
+}
