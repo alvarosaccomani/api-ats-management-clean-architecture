942a65c2536a641b7b7dfe028b239872c3805575
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-08 11:53:33 -0300

    i add bank.model.ts into bank folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/bank/bank.model.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { BankEntity } from "../../../domain/bank/bank.entity";
+
+export class SequelizeBank extends Model<BankEntity, Omit<BankEntity, 'id'>> {
+  declare ban_uuid: string;
+  declare ban_name: string;
+  declare ban_description: string;
+  declare ban_createdat: Date;
+  declare ban_updatedat: Date;
+}
+
+SequelizeBank.init({
+  ban_uuid: {
+    type: DataTypes.STRING, 
+    primaryKey: true,
+    allowNull: false
+  },
+  ban_name: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true
+  },
+  ban_description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  ban_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  ban_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  sequelize,
+  timestamps: true,
+  createdAt: 'ban_createdat',
+  updatedAt: 'ban_updatedat',
+  tableName: 'ban_banks'
+});
