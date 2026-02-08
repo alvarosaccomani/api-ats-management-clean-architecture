e5b066836bf2eeae6f7552beedb257b9794728a9
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-08 18:09:10 -0300

    i add voucher-state.value.ts into voucher-state folder into folder domain to project

---
+++ src/domain/voucher-state/voucher-state.value.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { VoucherStateEntity } from "./voucher-state.entity";
+
+export class VoucherStateValue implements VoucherStateEntity {
+    vous_uuid: string;
+    vous_name: string;
+    vous_description: string;
+    vous_createdat: Date;
+    vous_updatedat: Date;
+    
+    constructor({
+            vous_uuid,
+            vous_name,
+            vous_description,
+            vous_createdat,
+            vous_updatedat
+        }:{ 
+            vous_uuid?: string,
+            vous_name: string,
+            vous_description?: string,
+            vous_createdat?: Date,
+            vous_updatedat?: Date
+        }) {
+        this.vous_uuid = vous_uuid ?? uuid();
+        this.vous_name = vous_name;
+        this.vous_description = vous_description ?? '';
+        this.vous_createdat = vous_createdat ?? moment().toDate();
+        this.vous_updatedat = vous_updatedat ?? moment().toDate();
+    }
+}
