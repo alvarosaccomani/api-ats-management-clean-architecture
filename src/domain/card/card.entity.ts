328338f7ece221873fef0dbf734ee3645ab008de
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-08 16:17:09 -0300

    i add card.entity.ts into card folder into folder domain to project

---
+++ src/domain/card/card.entity.ts
@@ -0,0 +1,32 @@
+import { TypeCardEntity } from '../type-card/type-card.entity';
+import { BrandCardEntity } from '../brand-card/brand-card.entity';
+import { BankEntity } from '../bank/bank.entity';
+
+export interface CardEntity {
+    usr_uuid: string;
+    crd_uuid: string;
+    crd_maskedcardnumber: string;
+    crd_last4digits: string;
+    tycrd_uuid: string;
+    tycrd?: TypeCardEntity;
+    brcrd_uuid: string;
+    brcrd?: BrandCardEntity;
+    ban_uuid: string;
+    ban?: BankEntity;
+    crd_expirationdate: Date;
+    crd_creditlimit: number | null;
+    crd_active: boolean;
+    crd_createdat: Date;
+    crd_updatedat: Date;
+}
+
+export type CardUpdateData = Pick<CardEntity, 
+    'crd_maskedcardnumber' | 
+    'crd_last4digits' | 
+    'tycrd_uuid' | 
+    'brcrd_uuid' | 
+    'ban_uuid' | 
+    'crd_expirationdate' | 
+    'crd_creditlimit' | 
+    'crd_active'
+>;
