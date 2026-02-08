56a835351aefa44a7a1cd3f12cbc968b4c3a2437
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-08 11:56:34 -0300

    i add bank.route.ts into bank folder into folder router into folder infrastructure to project

---
+++ src/infrastructure/router/bank/bank.route.ts
@@ -0,0 +1,33 @@
+import { Express } from "express";
+import { SequelizeBankRepository } from "../../repository/bank/sequelize-bank.repository";
+import { BankUseCase } from "../../../application/bank/bank-use-case";
+import { BankController } from "../../controller/bank/bank.controller";
+import SocketAdapter from "../../services/socketAdapter";
+
+function configureBankRoutes(app: Express, socketAdapter: SocketAdapter) {
+    /*
+    *   Iniciar repository
+    */
+    
+    const sequelizeBankRepository = new SequelizeBankRepository();
+    
+    /*
+    *   Iniciar casos de uso
+    */
+    
+    const bankUseCase = new BankUseCase(sequelizeBankRepository);
+    
+    /*
+    *   Iniciar controller
+    */
+    
+    const bankCtrl = new BankController(bankUseCase, socketAdapter);
+    
+    app.get(`/${process.env.BASE_URL_API}/banks/:filter?/:page?/:perPage?`, bankCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/bank/:ban_uuid`, bankCtrl.getCtrl);
+    app.post(`/${process.env.BASE_URL_API}/bank`, bankCtrl.insertCtrl);
+    app.put(`/${process.env.BASE_URL_API}/bank/:ban_uuid`, bankCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/bank/:ban_uuid`, bankCtrl.deleteCtrl);
+}
+
+export default configureBankRoutes;
