e3d32837d977e537e54be24f714fb8c7f66600bf
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-08 11:52:04 -0300

    i add bank.value.ts into bank folder into folder domain to project

---
+++ src/domain/bank/bank.value.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { BankEntity } from "./bank.entity";
+
+export class BankValue implements BankEntity {
+    ban_uuid: string;
+    ban_name: string;
+    ban_description: string;
+    ban_createdat: Date;
+    ban_updatedat: Date;
+    
+    constructor({
+            ban_uuid,
+            ban_name,
+            ban_description,
+            ban_createdat,
+            ban_updatedat
+        }:{ 
+            ban_uuid?: string,
+            ban_name: string,
+            ban_description?: string,
+            ban_createdat?: Date,
+            ban_updatedat?: Date
+        }) {
+        this.ban_uuid = ban_uuid ?? uuid();
+        this.ban_name = ban_name;
+        this.ban_description = ban_description ?? '';
+        this.ban_createdat = ban_createdat ?? moment().toDate();
+        this.ban_updatedat = ban_updatedat ?? moment().toDate();
+    }
+}
