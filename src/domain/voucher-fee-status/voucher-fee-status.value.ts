d6beabdfb267bdb42f6b4432173f2beec0875d27
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-10 18:56:05 -0300

    i add voucher-fee-status.value.ts into voucher-fee-status folder into folder domain to project

---
+++ src/domain/voucher-fee-status/voucher-fee-status.value.ts
@@ -0,0 +1,31 @@
+import { v4 as uuid } from "uuid";
+import moment from 'moment';
+import { VoucherFeeStatusEntity } from "./voucher-fee-status.entity";
+
+export class VoucherFeeStatusValue implements VoucherFeeStatusEntity {
+    voufs_uuid: string;
+    voufs_name: string;
+    voufs_description: string;
+    voufs_createdat: Date;
+    voufs_updatedat: Date;
+    
+    constructor({
+            voufs_uuid,
+            voufs_name,
+            voufs_description,
+            voufs_createdat,
+            voufs_updatedat
+        }:{ 
+            voufs_uuid?: string,
+            voufs_name: string,
+            voufs_description?: string,
+            voufs_createdat?: Date,
+            voufs_updatedat?: Date
+        }) {
+        this.voufs_uuid = voufs_uuid ?? uuid();
+        this.voufs_name = voufs_name;
+        this.voufs_description = voufs_description ?? '';
+        this.voufs_createdat = voufs_createdat ?? moment().toDate();
+        this.voufs_updatedat = voufs_updatedat ?? moment().toDate();
+    }
+}
